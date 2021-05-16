0ea6d82dee82006160715d8848cdee9b53cd0127
Zareix <raphcatarino@gmail.com>
2021-05-17 01:12:53 +0200

    added archive (to finish)

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   import Config from "./routes/Config.svelte"
   import AddItem from "./routes/AddItem.svelte"
   import ProtectedRoutes from "./routes/ProtectedRoutes.svelte"
+  import Archive from "./routes/Archive.svelte"
 
   export let url = ""
 </script>
@@ -13,6 +14,7 @@
 <Router {url}>
   <Route path="login" component={Login} />
   <ProtectedRoutes path="settings" component={Config} />
+  <ProtectedRoutes path="archive" component={Archive} />
   <ProtectedRoutes path="add" component={AddItem} />
   <ProtectedRoutes path="item/:oldCategorie/:modifId" component={AddItem} />
   <ProtectedRoutes path="/" component={Home} />
---
+++ src/routes/Archive.svelte
@@ -0,0 +1,18 @@
+<script>
+  import TopBar from "../components/TopBar.svelte"
+
+  // TODO
+</script>
+
+<TopBar />
+<main>
+  <h1>Tous vos objets supprimés</h1>
+  In progress
+</main>
+
+<style>
+  h1 {
+    margin: 0 auto;
+    width: fit-content;
+  }
+</style>
