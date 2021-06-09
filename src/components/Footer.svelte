6e0c7c8420cd04d57f31376c19c6ec0ec87bc4f8
Zareix <raphcatarino@gmail.com>
2021-06-09 21:06:27 +0200

    added footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,50 @@
+<script>
+</script>
+
+<footer>
+  <img src="icons/icon-512x512.png" alt="wishlist logo" />
+  <div class="content">
+    <p>
+      Find this project on
+      <a href="https://github.com/Zareix/raphael-wishlist"> my Github</a><br />
+      Made by <em>Raphaël Catarino</em><br />
+      Learn more about me <a href="https://www.raphael-catarino.fr/">here</a>
+    </p>
+  </div>
+</footer>
+
+<style>
+  footer {
+    background-color: #f3f4f6;
+    color: #4b5563;
+    height: 175px;
+    padding-bottom: 1em;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: flex-end;
+    justify-content: center;
+    justify-items: center;
+  }
+
+  img {
+    width: 50px;
+    height: 50px;
+    margin-bottom: 0.75em;
+  }
+
+  a {
+    color: var(--gray);
+    text-decoration: underline;
+  }
+
+  p {
+    margin: 0 auto;
+    width: 70%;
+    line-height: 1.5em;
+    text-align: center;
+  }
+
+  .content {
+    width: 100%;
+  }
+</style>
